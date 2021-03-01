0c1540537d3fbbe396f401cea4e0ecb2bf457098
Manuel Jose Sotomayor Torrealba <manuel.sotomayor@despegar.com>
2021-03-01 15:27:34 -0300

    probando

--- send_notification.js
+++ send_notification.js
@@ -6,18 +6,19 @@ let sendNotification = function () {
     return new Promise((resolve, reject) => {
         try {
 
-            const {repo} = github.context.repo
-            const pullRequestPayload = github.context.payload
-            const pullRequest = pullRequestPayload.pull_request
-            var data = '{"text" : "['+repo+'] - PR '+pull_request.user.login+' opened '+pullRequest.html_url+'  ('+pullRequest.title+')"}';
-            var config = {
-              method: 'post',
-              url: 'https://chat.googleapis.com/v1/spaces/AAAA7OKsf0M/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=2tWgZ47_zVfbjD96NyjOCHQqJapONCVDTSro4uhkwd8%3D',
-              headers: { 
-                'Content-Type': 'application/json; charset=UTF-8'
-              },
-              data : data
-            };
+          const {owner, repo} = github.context.repo
+          const pullRequestPayload = github.context.payload
+          const pullRequest = pullRequestPayload.pull_request
+          var data = '{"text" : "'+owner+' - PR '+repo+' '+pullRequest.html_url+'  ('+pullRequest.title+')"}';
+
+          var config = {
+            method: 'post',
+            url: 'https://chat.googleapis.com/v1/spaces/AAAA7OKsf0M/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=2tWgZ47_zVfbjD96NyjOCHQqJapONCVDTSro4uhkwd8%3D',
+            headers: { 
+              'Content-Type': 'application/json; charset=UTF-8'
+            },
+            data : data
+          };
             
             let response = axios(config)
             console.log(github.context)
